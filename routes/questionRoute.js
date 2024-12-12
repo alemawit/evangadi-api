0ee833ab0bb2286638d2b79c4235a971dc589134
Mihretf <158094064+Mihretf@users.noreply.github.com>
2024-12-12 19:40:35 +0300

    question route and controller

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,1 +1,17 @@
 // question routes
+const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const router = express.Router();
+// Question controllers
+const {
+  postQuestion,
+  allQuestion,
+  singleQuestion,
+} = require("../controller/questionController");
+// Post a new question
+router.post("", authMiddleware, postQuestion);
+// Get all questions
+router.get("", authMiddleware, allQuestion);
+// Get a single question by ID
+router.get("/:questionid", authMiddleware, singleQuestion);
+module.exports = router;
